1703c432f80335a78bc46b26c3731cc84b341c7a
fffk10 <warafffk10@gmail.com>
2023-04-08 21:59:13 +0900

    🎉 Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // postcssの設定
+  postcssLoaderOptions: {
+    postcssOptions: {
+      config: './postcss.config.js',
+    },
+  },
 }
 
 module.exports = nextConfig
